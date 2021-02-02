166f2302877d33f5d04029801fb1d3f46ed3fe49
Kyle Brindley <kbrindley@lanl.gov>
2021-02-02 16:57:01 -0700

    FEAT: use vector tools to perform two dimensional array equality check

--- src/cpp/tests/test_umat.cpp
+++ src/cpp/tests/test_umat.cpp
@@ -27,5 +27,5 @@ BOOST_AUTO_TEST_CASE( testColumnToRowMajor ){
     std::vector< std::vector< double > > answer = { { 1, 2, 3 },
                                                     { 4, 5, 6 } };
 
-    BOOST_CHECK( row_major == answer );
+    BOOST_CHECK( vectorTools::fuzzyEquals( row_major, answer ) );
 }
