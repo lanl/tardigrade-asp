209ee000135633f1b806b98d77b84e6eb47694a6
Kyle Andrew Brindley <kbrindley@lanl.gov>
2021-02-02 10:51:51 -0700

    WIP: compilable umat tests

--- src/cpp/tests/test_umat.cpp
+++ src/cpp/tests/test_umat.cpp
@@ -6,7 +6,7 @@
 
 #include<umat.h>
 
-#define BOOST_TEST_MODULE test_cpp_stub
+#define BOOST_TEST_MODULE test_umat
 #include <boost/test/included/unit_test.hpp>
 
 BOOST_AUTO_TEST_CASE( testColumnToRowMajor ){
@@ -14,15 +14,16 @@ BOOST_AUTO_TEST_CASE( testColumnToRowMajor ){
      * Test column to row major conversion function.
      * Uses c++ arrays to avoid adding fortran code to project.
      */
-    std::vector< std::vector < double > > column_major = { { 1, 4 },
-                                                           { 2, 5 },
-                                                           { 3, 6 } };
+    std::vector < double > column_major = { 1, 4,
+                                            2, 5, 
+                                            3, 6 };
+    double *column_major_pointer = &column_major[0];
     const int width = 3;
     const int height = 2;
     std::vector< std::vector< double > > row_major;
-    row_major = umat::columnToRowMajor( column_major, width, height); 
+    row_major = columnToRowMajor( column_major_pointer, width, height); 
     std::vector< std::vector< double > > answer = { { 1, 2, 3 },
                                                     { 4, 5, 6 } };
 
-    BOOST_CHECK( result.is_equal( answer ) );
+    BOOST_CHECK( row_major == answer );
 }
