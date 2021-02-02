3fc2d7265d8333873fc6c65827441e17f09b28ea
Kyle Andrew Brindley <kbrindley@lanl.gov>
2021-02-02 11:13:54 -0700

    WIP: tests pass, but I'm not confident that I'm performing the right test

--- src/cpp/tests/test_umat.cpp
+++ src/cpp/tests/test_umat.cpp
@@ -14,9 +14,12 @@ BOOST_AUTO_TEST_CASE( testColumnToRowMajor ){
      * Test column to row major conversion function.
      * Uses c++ arrays to avoid adding fortran code to project.
      */
-    std::vector < double > column_major = { 1, 4,
-                                            2, 5, 
-                                            3, 6 };
+//    std::vector < double > column_major = { 1, 4,
+//                                            2, 5, 
+//                                            3, 6 };
+    std::vector < double > column_major = { 1, 2,
+                                            3, 4, 
+                                            5, 6 };
     double *column_major_pointer = &column_major[0];
     const int width = 3;
     const int height = 2;
@@ -24,6 +27,26 @@ BOOST_AUTO_TEST_CASE( testColumnToRowMajor ){
     row_major = columnToRowMajor( column_major_pointer, width, height); 
     std::vector< std::vector< double > > answer = { { 1, 2, 3 },
                                                     { 4, 5, 6 } };
+
+    //DEBUGGING
+    int column_major_index;
+    std::cout << "column_major_pointer" << std::endl;
+    for (int row = 0; row < height; row++){
+        for (int col = 0; col < width; col++){
+            column_major_index = row*width + col;
+            std::cout << column_major_pointer[column_major_index] << " ";
+        }
+        std::cout << std::endl;
+    }
+    std::cout << std::endl;
+    std::cout << "row_major" << std::endl;
+    for (int row = 0; row < height; row++){
+        for (int col = 0; col < width; col++){
+            std::cout << row_major[row][col] << " ";
+        }
+        std::cout << std::endl;
+    }
+    std::cout << std::endl;
 
     BOOST_CHECK( row_major == answer );
 }
