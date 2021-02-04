0a01d0e2f2687a6a30a6450443455b513fc6f52f
Kyle Brindley <kbrindley@lanl.gov>
2021-02-04 16:36:17 -0700

    WIP: add cpp hello world use to umat interface

--- src/cpp/umat.cpp
+++ src/cpp/umat.cpp
@@ -86,6 +86,8 @@ extern "C" void umat_( double *STRESS,       double *STATEV,       double *DDSDD
     const std::vector< std::vector< double > > dfgrd0 = columnToRowMajor( DFGRD0, 3, 3 );
     const std::vector< std::vector< double > > dfgrd1 = columnToRowMajor( DFGRD1, 3, 3 );
 
+    cppStub::sayHello("Abaqus");
+
     return;
 }
 
