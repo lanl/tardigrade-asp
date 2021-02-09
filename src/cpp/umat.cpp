cdf5a76c74848494ce55015450519275bccb6573
Kyle Brindley <kbrindley@lanl.gov>
2021-02-09 16:50:05 -0700

    FEAT: fixed templated function declaration/definition. headers *mumble* templates *mumble* *distant screams of frustration* https://stackoverflow.com/questions/495021/why-can-templates-only-be-implemented-in-the-header-file; https://www.reddit.com/r/programmingmemes/comments/l2638z/confused_unga_bunga/

--- src/cpp/umat.cpp
+++ src/cpp/umat.cpp
@@ -113,55 +113,6 @@ extern "C" void umat_( double *STRESS,       double *STATEV,       double *DDSDD
     return;
 }
 
-template< typename T >
-std::vector< std::vector< T > > columnToRowMajor( const T *column_major, const int &width, const int &height ){
-    /*!
-     * Convert column major two dimensional arrays to row major.
-     *
-     * Specifically, convert pointers to Fortran column major arrays to c++ row major vector of vectors.
-     *
-     * \param *column_major: The pointer to the start of a column major array
-     * \param &width: The width of the array, e.g. number of columns
-     * \param &height: The height of the array, e.g. number of rows
-     * \return row_major: A c++ row major vector of vectors
-     */
-    std::vector< std::vector< T > > row_major;
-    int column_major_index;
-    for ( int row = 0; row < height; row++ ){
-        std::vector< T > row_vector;
-        for ( int col = 0; col < width; col++ ){
-            column_major_index = col*height + row;
-            row_vector.push_back( *( column_major + column_major_index ) );
-        }
-        row_major.push_back( row_vector );
-    }
-    return row_major;
-}
-
-void rowToColumnMajor( double *column_major, const std::vector< std::vector< double > > &row_major_array,
-                       const int &width, const int &height){
-    /*!
-     * Convert row major two dimensional arrays to column major
-     *
-     * Specifically, c++ row major vector or vectors or arrays to Fortran column major arrays using the column major
-     * pointer.
-     *
-     * \param *column_major: The pointer to the start of a column major array
-     * \param &row_major_array: A c++ two dimensional, row major vector of vectors
-     * \param &width: The width of the array, e.g. number of columns
-     * \param &height: The height of the array, e.g. number of rows
-     */
-    int column_major_index;
-    for ( int row = 0; row < height; row++ ){
-        for ( int col = 0; col < width; col++ ){
-            column_major_index = col*height + row;
-            column_major[column_major_index] = row_major_array[row][col];
-        }
-    }
-
-    return;
-}
-
 char *FtoCString( int stringLength, const char* fString ){
     /*!
      * Converts a Fortran string to C-string. Trims trailing white space during processing.
