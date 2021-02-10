f9d884807803d2f9c2d0d662c5b0998361b5af93
Kyle Brindley <kbrindley@lanl.gov>
2021-02-09 17:16:30 -0700

    TST: add tests for c++ array stored as row major vector to fortran array conversion

--- src/cpp/tests/test_umat.cpp
+++ src/cpp/tests/test_umat.cpp
@@ -36,17 +36,37 @@ BOOST_AUTO_TEST_CASE( testRowToColumnMajor ){
      * Uses c++ vectors and pointers to avoid adding fortran code to project.
      */
 
+    //Test the interface using a c++ vector of vectors
     //Fake a Fortran column major array in memory with a c++ row major vector
     std::vector< double > column_major(6);
-    double *column_major_pointer = column_major.data();
+    double *column_major_pointer = column_major.data( );
+    std::vector< double > expected = { 1, 4,
+                                       2, 5,
+                                       3, 6 };
     const int width = 3;
     const int height = 2;
     std::vector< std::vector< double > > row_major = { { 1, 2, 3 },
                                                        { 4, 5, 6 } };
     rowToColumnMajor( column_major_pointer, row_major, width, height );
-    std::vector< double > expected = { 1, 4,
-                                       2, 5,
-                                       3, 6 };
 
     BOOST_CHECK( vectorTools::fuzzyEquals( column_major, expected ) );
+
+    //Test the interface using a c++ vector saved in row major order
+    //Reset fake Fortran column major array
+    column_major = { 0, 0, 0, 0, 0, 0 };
+    std::vector< double > row_major_vector = { 1, 2, 3,
+                                               4, 5, 6 };
+    rowToColumnMajor( column_major_pointer, row_major_vector, width, height );
+
+    BOOST_CHECK( vectorTools::fuzzyEquals( column_major, expected ) );
+
+    //Test a single row to single column
+    //Create a fake Fortran vector in memory with a c++ row major vector
+    std::vector< double > fortran_vector(3);
+    double *fortran_vector_pointer = fortran_vector.data( );
+    std::vector< double > expected_vector = { 1, 2, 3 };
+    std::vector< double > cpp_vector = { 1, 2, 3 };
+    rowToColumnMajor( fortran_vector_pointer, cpp_vector, 3, 1 );
+
+    BOOST_CHECK( vectorTools::fuzzyEquals( fortran_vector, expected_vector ) );
 }
