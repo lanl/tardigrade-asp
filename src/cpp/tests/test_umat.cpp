c454379205468f298ed786708ba6de54ecea5187
Kyle Brindley <kbrindley@lanl.gov>
2021-02-10 16:01:00 -0700

    TST: add function test for expanding abaqus standard stress type vectors

--- src/cpp/tests/test_umat.cpp
+++ src/cpp/tests/test_umat.cpp
@@ -70,3 +70,51 @@ BOOST_AUTO_TEST_CASE( testRowToColumnMajor ){
 
     BOOST_CHECK( vectorTools::fuzzyEquals( fortran_vector, expected_vector ) );
 }
+
+BOOST_AUTO_TEST_CASE( expandAbaqusStandardStressVector ){
+    /*!
+     * Test expansion of stress and strain type components to full Abaqus vectors
+     *
+     * See the Abaqus documentation > Introduction & Spatial Modeling > Conventions chapter > Convention used for stress
+     * and strain components.
+     *
+     * The stress vector components for Abaqus/Standard (UMAT) are
+     *
+     * \f$ \left { \sigma_{11}, \sigma_{22}, \sigma_{33}, \tau_{12}, \tau_{13}, \tau_{23} } \f$
+     *
+     * and the strain vector components match as
+     *
+     * \f$ \left { \epsilon_{11}, \epsilon_{22}, \epsilon_{33}, \gamma_{12}, \gamma_{13}, \gamma_{23} } \f$
+     *
+     * where components that are zero-valued by definition, e.g. plane stress, are omitted. The shear strain is the
+     * engineering shear strain where
+     *
+     * \f$ \gamma_{ij} = \epsilon_{ij} + \epsilon_{ji} \f$
+     */
+
+     //Initialize common test variables
+     int NDI;
+     int NSHR;
+     std::vector< double > vector_expansion(6, -666.);
+
+     //Test full size vector
+     std::vector< double > abaqus_full = { 11, 22, 33, 12, 13, 23 };
+     std::vector< double > expected_full = { 11, 22, 33, 12, 13, 23 };
+     NDI = 3;
+     NSHR = 3;
+
+     vector_expansion = expandAbaqusStandardStressVector( abaqus_full, &NDI, &NSHR );
+
+     BOOST_CHECK( vectorTools::FuzzyEquals( vector_expansion, expected_full );
+
+     //Test plane stress vector
+     std::fill(vector_expansion.begin(), vector_expansion.end(), -666.);
+     std::vector< double > abaqus_plane_stress = { 11, 22, 12 };
+     std::vector< double > expected_plane_stress = { 11, 22, 0., 12, 0., 0. };
+     NDI = 2;
+     NSHR = 1;
+
+     vector_expansion = expandAbaqusStandardStressVector( abaqus_full, NDI, NSHR );
+
+     BOOST_CHECK( vectorTools::FuzzyEquals( vector_expansion, expected_full );
+}
