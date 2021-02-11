5af60a7ee6a8ed0c8cb0e10044031dc1d816e771
Kyle Brindley <kbrindley@lanl.gov>
2021-02-10 18:28:30 -0700

    WIP: use upper/lower case variables for consistency with umat subroutine and add minimal doc string

--- src/cpp/cpp_stub.cpp
+++ src/cpp/cpp_stub.cpp
@@ -16,14 +16,19 @@ namespace cppStub{
         std::cout << "Hello " << message << std::endl;
     }
 
-    void abaqusInterface( floatVector &STRESS,       floatVector &STATEV,       floatVector &DDSDDE,       floatType &SSE,           floatType &SPD,
-                          floatType &SCD,            floatType &RPL,            floatVector &DDSDDT,       floatVector &DRPLDE,      floatType &DRPLDT,
-                          const floatVector &STRAN,  const floatVector &DSTRAN, const floatVector &TIME,   const floatType &DTIME,   const floatType &TEMP,
-                          const floatType &DTEMP,    const floatVector &PREDEF, const floatVector &DPRED,  const char *CMNAME,       const int &NDI,
-                          const int &NSHR,           const int &NTENS,          const int &NSTATV,         const floatVector &PROPS, const int &NPROPS,
-                          const floatVector &COORDS, const floatVector &DROT,   floatType &PNEWDT,         const floatType &CELENT,  const floatVector &DFGRD0,
-                          const floatVector &DFGRD1, const int &NOEL,           const int &NPT,            const int &LAYER,         const int &KSPT,
-                          const int *JSTEP,          const int &KINC ){
+    void abaqusInterface( floatVector &stress,       floatVector &statev,        floatVector &ddsdde,       floatType &SSE,           floatType &SPD,
+                          floatType &SCD,            floatType &RPL,             floatVector &ddsddt,       floatVector &drplde,      floatType &DRPLDT,
+                          const floatVector &strain, const floatVector &dstrain, const floatVector &time,   const floatType &DTIME,   const floatType &TEMP,
+                          const floatType &DTEMP,    const floatVector &predef,  const floatVector &dpred,  const char &cmname,       const int &NDI,
+                          const int &NSHR,           const int &NTENS,           const int &NSTATV,         const floatVector &props, const int &NPROPS,
+                          const floatVector &coords, const floatVector &drot,    floatType &PNEWDT,         const floatType &CELENT,  const floatVector &dfgrd0,
+                          const floatVector &dfgrd1, const int &NOEL,            const int &NPT,            const int &LAYER,         const int &KSPT,
+                          const int &jstep,          const int &KINC ){
+        /*!
+         * A template Abaqus UMAT c++ interface using c++ STL types. Variables in all caps reference ABAQUS FORTRAN
+         * memory directly. Variables in lower case are native c++ type conversions stored separately from the original
+         * ABAQUS FORTRAN memory.
+         */
 
         return;
     }
