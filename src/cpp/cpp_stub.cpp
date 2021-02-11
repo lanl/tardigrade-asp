a6753506d9d06ba625fbe03cc1f9a78b8805e51d
Kyle Brindley <kbrindley@lanl.gov>
2021-02-11 14:13:14 -0700

    WIP: move entire abaqus interface into project source. Preparing to use umat as a pass through logic structure only

--- src/cpp/cpp_stub.cpp
+++ src/cpp/cpp_stub.cpp
@@ -16,28 +16,84 @@ namespace cppStub{
         std::cout << "Hello " << message << std::endl;
     }
 
-    void abaqusInterface( floatVector &stress,             floatVector &statev,        floatMatrix &ddsdde,       floatType &SSE,            floatType &SPD,
-                          floatType &SCD,                  floatType &RPL,             floatVector &ddsddt,       floatVector &drplde,       floatType &DRPLDT,
-                          const floatVector &strain,       const floatVector &dstrain, const floatVector &time,   const floatType &DTIME,    const floatType &TEMP,
-                          const floatType &DTEMP,          const floatVector &predef,  const floatVector &dpred,  const std::string &cmname, const int &NDI,
-                          const int &NSHR,                 const int &NTENS,           const int &NSTATV,         const floatVector &props,  const int &NPROPS,
-                          const floatVector &coords,       const floatMatrix &drot,    floatType &PNEWDT,         const floatType &CELENT,   const floatMatrix &dfgrd0,
-                          const floatMatrix &dfgrd1,       const int &NOEL,            const int &NPT,            const int &LAYER,          const int &KSPT,
-                          const std::vector< int > &jstep, const int &KINC ){
+    void abaqusInterface( double *STRESS,       double *STATEV,       double *DDSDDE,       double &SSE,          double &SPD,
+                          double &SCD,          double &RPL,          double *DDSDDT,       double *DRPLDE,       double &DRPLDT,
+                          const double *STRAN,  const double *DSTRAN, const double *TIME,   const double &DTIME,  const double &TEMP,
+                          const double &DTEMP,  const double *PREDEF, const double *DPRED,  const char *CMNAME,   const int &NDI,
+                          const int &NSHR,      const int &NTENS,     const int &NSTATV,    const double *PROPS,  const int &NPROPS,
+                          const double *COORDS, const double *DROT,   double &PNEWDT,       const double &CELENT, const double *DFGRD0,
+                          const double *DFGRD1, const int &NOEL,      const int &NPT,       const int &LAYER,     const int &KSPT,
+                          const int *JSTEP,     const int &KINC ){
+    /*!
+     * A template Abaqus UMAT c++ interface that performs Fortran to C++ type conversions, calculates the material
+     * model's expected input, handles tensor shape changes, and calls a c++ material model.
+     */
+
+    //Define the tensor spatial dimensions
+    const int spatialDimensions = 3;
+
+    //Map FORTRAN UMAT variables to C++ types as necessary. Use case sensitivity to distinguish.
+    //TODO: Decide if case sensitive variable names is a terrible idea or not
+    //Vectors can be created directly with pointer arithmetic
+    std::vector< double > stress( STRESS, STRESS + NTENS );
+    std::vector< double > statev( STATEV, STATEV + NSTATV );
+    std::vector< double > ddsddt( DDSDDT, DDSDDT + NTENS );
+    std::vector< double > drplde( DRPLDE, DRPLDE + NTENS );
+    const std::vector< double > strain( STRAN, STRAN + NTENS );
+    const std::vector< double > dstrain( DSTRAN, DSTRAN + NTENS );
+    const std::vector< double > time( TIME, TIME + 2 );
+    const std::vector< double > predef( PREDEF, PREDEF + 1 );
+    const std::vector< double > dpred( DPRED, DPRED + 1 );
+    const std::string cmname( abaqusTools::FtoCString( 80, CMNAME ) );
+    const std::vector< double > props( PROPS, PROPS + NPROPS );
+    const std::vector< double > coords( COORDS, COORDS + spatialDimensions );
+    const std::vector< int > jstep( JSTEP, JSTEP + 4 );
+    //Fortran two-dimensional arrays require careful column to row major conversions to c++ types
+    std::vector< std::vector< double > > ddsdde = abaqusTools::columnToRowMajor( DDSDDE, NTENS, NTENS );
+    const std::vector< std::vector< double > > drot = abaqusTools::columnToRowMajor( DROT, spatialDimensions, spatialDimensions );
+    const std::vector< std::vector< double > > dfgrd0 = abaqusTools::columnToRowMajor( DFGRD0, spatialDimensions, spatialDimensions );
+    const std::vector< std::vector< double > > dfgrd1 = abaqusTools::columnToRowMajor( DFGRD1, spatialDimensions, spatialDimensions );
+
+    //Call the constitutive model c++ interface
+    if ( KINC == 1 && NOEL == 1 && NPT == 1 ){
+        dummyMaterialModel( stress, statev,  ddsdde, SSE,    SPD,
+                            SCD,    RPL,     ddsddt, drplde, DRPLDT,
+                            strain, dstrain, time,   DTIME,  TEMP,
+                            DTEMP,  predef,  dpred,  cmname, NDI,
+                            NSHR,   NTENS,   NSTATV, props,  NPROPS,
+                            coords, drot,    PNEWDT, CELENT, dfgrd0,
+                            dfgrd1, NOEL,    NPT,    LAYER,  KSPT,
+                            jstep,  KINC );
+    }
+
+    //Re-pack C++ objects into FORTRAN memory to return values to Abaqus
+    //Scalars were passed by reference and will update correctly
+    //Vectors don't require row/column major considerations, but do require re-packing to the Fortran pointer
+    abaqusTools::rowToColumnMajor( STRESS, stress, 1, NTENS );
+    abaqusTools::rowToColumnMajor( DDSDDT, ddsddt, 1, NTENS );
+    abaqusTools::rowToColumnMajor( DRPLDE, drplde, 1, NTENS );
+    abaqusTools::rowToColumnMajor( STATEV, statev, 1, NSTATV );
+    //Arrays require vector of vector to column major conversion
+    abaqusTools::rowToColumnMajor(DDSDDE, ddsdde, spatialDimensions, spatialDimensions);
+
+    }
+
+    void dummyMaterialModel( floatVector &stress,             floatVector &statev,        floatMatrix &ddsdde,       floatType &SSE,            floatType &SPD,
+                             floatType &SCD,                  floatType &RPL,             floatVector &ddsddt,       floatVector &drplde,       floatType &DRPLDT,
+                             const floatVector &strain,       const floatVector &dstrain, const floatVector &time,   const floatType &DTIME,    const floatType &TEMP,
+                             const floatType &DTEMP,          const floatVector &predef,  const floatVector &dpred,  const std::string &cmname, const int &NDI,
+                             const int &NSHR,                 const int &NTENS,           const int &NSTATV,         const floatVector &props,  const int &NPROPS,
+                             const floatVector &coords,       const floatMatrix &drot,    floatType &PNEWDT,         const floatType &CELENT,   const floatMatrix &dfgrd0,
+                             const floatMatrix &dfgrd1,       const int &NOEL,            const int &NPT,            const int &LAYER,          const int &KSPT,
+                             const std::vector< int > &jstep, const int &KINC ){
         /*!
-         * A template Abaqus UMAT c++ interface using c++ STL types. Variables in all caps reference ABAQUS FORTRAN
+         * A template Abaqus c++ UMAT using c++ STL types. Variables in all caps reference ABAQUS FORTRAN
          * memory directly. Variables in lower case are native c++ type conversions stored separately from the original
          * ABAQUS FORTRAN memory.
          */
 
-        //Perform type conversions and variable calculations for constitutive model interface
-        //Common example conversions is to expand NTENS vectors (stress, strain, etc) and arrays (ddsdde) to the Abaqus
-        //Voigt notation in 1x6 or 6x6 shape.
-
-        //Call constitutive model
+        //Call functions of constitutive model to do things
         sayHello( "Abaqus" );
-
-        //Re-pack original c++ UMAT variables (lower case) from constitutive model interface
 
         return;
     }
