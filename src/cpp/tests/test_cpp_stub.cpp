a421ef34b6834022999576745aa64a0ab6fb32df
Kyle Brindley <kbrindley@lanl.gov>
2021-04-19 15:15:46 -0600

    WIP: add tests for abaqus interface

--- src/cpp/tests/test_cpp_stub.cpp
+++ src/cpp/tests/test_cpp_stub.cpp
@@ -45,3 +45,122 @@ BOOST_AUTO_TEST_CASE( testSayHello ){
     BOOST_CHECK( result.is_equal( answer ) );
 
 }
+
+BOOST_AUTO_TEST_CASE( testAbaqusInterface ){
+    /*!
+     * Test the cpp_stub abaqus interface
+     */
+
+    double double_scalar = 0.0;
+    int int_scalar = 0;
+
+    //Create nominally correct variable holders that match expected Abaqus Fortran interface
+    //TODO: fill out nominally correct variable shape and values
+    //Strings
+    char CMNAME[ ] = "cpp_stub";
+    //Scalar integers
+    int NDI = 3;
+    int NSHR = 3;
+    int NTENS = 6;
+    int NSTATV = 1;
+    int NPROPS = 1;
+    int NOEL = int_scalar;
+    int NPT = int_scalar;
+    int LAYER = int_scalar;
+    int KSPT = int_scalar;
+    int KINC = int_scalar;
+    //Scalar doubles
+    double SSE = double_scalar;
+    double SPD = double_scalar;
+    double SCD = double_scalar;
+    double RPL = double_scalar;
+    double DRPLDT = double_scalar;
+    double DTIME = double_scalar;
+    double TEMP = double_scalar;
+    double DTEMP = double_scalar;
+    double PNEWDT = double_scalar;
+    double CELENT = double_scalar;
+    //Fortan int column major arrays
+    std::vector< int > jstep( 4 );
+    int* JSTEP  = jstep.data( );
+    //Fortan double column major arrays
+    std::vector< double > stress( NTENS );
+    double* STRESS = stress.data( );
+    std::vector< double > statev( NSTATV );
+    double* STATEV = statev.data( );
+    std::vector< double > ddsdde( NTENS * NTENS );
+    double* DDSDDE = ddsdde.data( );
+    std::vector< double > ddsddt( NTENS );
+    double* DDSDDT = ddsddt.data( );
+    std::vector< double > drplde( NTENS );
+    double* DRPLDE = drplde.data( );
+    std::vector< double > strain( NTENS );
+    double* STRAN  = strain.data( );
+    std::vector< double > dstrain( NTENS );
+    double* DSTRAN = dstrain.data( );
+    std::vector< double > time( 2 );
+    double* TIME   = time.data( );
+    std::vector< double > predef( 1 );
+    double* PREDEF = predef.data( );
+    std::vector< double > dpred( 1 );
+    double* DPRED  = dpred.data( );
+    std::vector< double > props( NPROPS );
+    double* PROPS  = props.data( );
+    //TODO: figure out how to use cpp_stub::spatialDimensions here
+    std::vector< double > coords( 3 );
+    double* COORDS = coords.data( );
+    //TODO: figure out how to use cpp_stub::spatialDimensions here
+    std::vector< double > drot( 3 * 3);
+    double* DROT   = drot.data( );
+    //TODO: figure out how to use cpp_stub::spatialDimensions here
+    std::vector< double > dfgrd0( 3 * 3);
+    double* DFGRD0 = dfgrd0.data( );
+    //TODO: figure out how to use cpp_stub::spatialDimensions here
+    std::vector< double > dfgrd1( 3 * 3);
+    double* DFGRD1 = dfgrd1.data( );
+
+    //Sign of life test. Just run to see if any exceptions are thrown.
+    cpp_stub::abaqusInterface(
+        STRESS, STATEV, DDSDDE, SSE,    SPD,
+        SCD,    RPL,    DDSDDT, DRPLDE, DRPLDT,
+        STRAN,  DSTRAN, TIME,   DTIME,  TEMP,
+        DTEMP,  PREDEF, DPRED,  CMNAME, NDI,
+        NSHR,   NTENS,  NSTATV, PROPS,  NPROPS,
+        COORDS, DROT,   PNEWDT, CELENT, DFGRD0,
+        DFGRD1, NOEL,   NPT,    LAYER,  KSPT,
+        JSTEP,  KINC );
+
+    //Check for nStateVariables thrown exception
+    std::vector< double > temp = { 1 };
+    double* STATEV_incorrect = temp.data( );
+    int NSTATV_incorrect = temp.size( );
+    BOOST_CHECK_THROW(
+        cpp_stub::abaqusInterface(
+           STRESS, STATEV_incorrect, DDSDDE,           SSE,    SPD,
+           SCD,    RPL,              DDSDDT,           DRPLDE, DRPLDT,
+           STRAN,  DSTRAN,           TIME,             DTIME,  TEMP,
+           DTEMP,  PREDEF,           DPRED,            CMNAME, NDI,
+           NSHR,   NTENS,            NSTATV_incorrect, PROPS,  NPROPS,
+           COORDS, DROT,             PNEWDT,           CELENT, DFGRD0,
+           DFGRD1, NOEL,             NPT,              LAYER,  KSPT,
+           JSTEP,  KINC ),
+        std::exception );
+
+    //Check for nMaterialParameters thrown exception
+    temp = { 1 };
+    double* PROPS_incorrect = temp.data( );
+    int NPROPS_incorrect = temp.size( );
+
+    BOOST_CHECK_THROW(
+        cpp_stub::abaqusInterface(
+           STRESS, STATEV, DDSDDE, SSE,             SPD,
+           SCD,    RPL,    DDSDDT, DRPLDE,          DRPLDT,
+           STRAN,  DSTRAN, TIME,   DTIME,           TEMP,
+           DTEMP,  PREDEF, DPRED,  CMNAME,          NDI,
+           NSHR,   NTENS,  NSTATV, PROPS_incorrect, NPROPS_incorrect,
+           COORDS, DROT,   PNEWDT, CELENT,          DFGRD0,
+           DFGRD1, NOEL,   NPT,    LAYER,           KSPT,
+           JSTEP,  KINC ),
+        std::exception );
+
+}
