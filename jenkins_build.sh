f168d1600ee00754d752286a1239ce6ed165bf14
Kyle Andrew Brindley <kbrindley@lanl.gov>
2021-05-20 14:10:39 -0600

    MAINT: update jenkins CI script for multiple environments and hosts

--- jenkins_build.sh
+++ jenkins_build.sh
@@ -1,32 +1,28 @@
 #!/usr/bin/env bash
 
-# Identify W-13 specific common paths automatically
-case $OSTYPE in
-    darwin*)
-        apps="/Users/apps"
-        projects="/Users/projects"
-        ;;
-    linux-gnu*)
-        apps="/apps"
-        projects="/projects"
-        ;;
-    *)
-        echo "Detected OS $OSTYPE is not supported. Exiting."
-        exit 1
+set -x
+
+# Test dev branch against beta environment. All other branches against release
+environment='release'
+env_alias='sv3r'
+if [ ${targetBranch} == dev ]; then
+    environment='beta'
+    env_alias='sv3b'
+fi
+
+# Activate W-13 Python environment
+case $(hostname) in
+    sstelmo.lanl.gov|mayhem.lanl.gov)
+        module load python/2020.07-python-3.8
+        ${env_alias}
+        export PATH=$PATH:/apps/abaqus/Commands/
+        ;;  # No fall through
+    sn-fey?.lanl.gov|sn-rfe?.lanl.gov|sn???.lanl.gov)
+        module load python/3.8-anaconda-2020.07
+        source activate /usr/projects/ea/python/${environment}
+        export PATH=$PATH:/usr/projects/ea/abaqus/Commands/
         ;;
 esac
-
-# Activate conda environment
-rel='release'
-if [ -f "${apps}/anaconda/2019.10-python-3.7/etc/profile.d/conda.sh" ]; then
-    . "${apps}/anaconda/2019.10-python-3.7/etc/profile.d/conda.sh"
-    conda activate
-    conda activate "${projects}/python/${rel}"
-else
-    export PATH="${apps}/anaconda/5.0.1-python-3.6/bin:$PATH"
-    source activate
-    source activate "${projects}/python/${rel}"
-fi
 
 # Make bash script more like high-level languages.
 set -Eeuxo pipefail
@@ -34,51 +30,8 @@ set -Eeuxo pipefail
 # report conda environment
 conda info
 
-# Debugging
-whoami
-groups
-ls -l $HOME/include || true
-ls -l $HOME/.local/include || true
+# Clean and build project
+./BUILD.sh
 
-# Save working directory
-workdir=${PWD}
-
-#================================================= TEST AGAINST g++ or c++ COMPILER ===
-case $OSTYPE in
-    darwin*)
-        compiler='c++'
-        ;;
-    linux-gnu*)
-        compiler='g++'
-        ;;
-esac
-./new_build.sh ${compiler} 4
-
-# Perform repo tests
-cd "build"
-ctest --verbose --output-log results.tex
-
-# Append the path of the compiler
-echo "$(command -v ${compiler})" >> results.tex
-echo -e "$(${compiler} --version)" >> results.tex
-# Rename results according to compiler
-mv results.tex "${compiler}_results.tex"
-
-# Return to working directory
-cd ${workdir}
-cp "build/${compiler}_results.tex" .
-
-#============================================================= CHECK RESULTS ===
-# Check for failed tests
-cd ${workdir}
-result_files=(`find . -maxdepth 1 -name "*_results.tex"`)
-echo "Result files: ${result_files[@]}"
-echo "Number of results files found: ${#result_files[@]}"
-
-# Check number of results files to check find globbing
-# TODO: fix the expected compiler count by OSTYPE or HOSTNAME
-if [ ${#result_files[@]} -lt 1 ]; then
-    echo "Found the wrong number of result files: ${#result_files[@]}"
-    echo "result files found: ${result_files[@]}"
-    exit 2
-fi
+# Run project tests
+./TEST.sh
