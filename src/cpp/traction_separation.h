a0b5a009aa03644aff0761e228aae06cba4d4f4f
Nathan Andrew Miller <nathanm@lanl.gov>
2023-02-14 10:43:32 -0700

    FEAT: Added more general distance calculation

--- src/cpp/traction_separation.h
+++ src/cpp/traction_separation.h
@@ -53,6 +53,15 @@ namespace tractionSeparation{
                                             floatMatrix &dddXi_1, floatMatrix &dddXi_2, floatMatrix &dddD,
                                             floatMatrix &dddF, floatMatrix &dddchi, floatMatrix &dddchiNL );
 
+    errorOut computeCurrentDistanceGeneral( const floatVector &Xi_1, const floatVector &Xi_2, const floatVector &D,
+                                            const floatVector &F,    const floatVector &chi,  const floatVector &chiNL,
+                                            floatVector &d,
+                                            floatMatrix &dddXi_1, floatMatrix &dddXi_2, floatMatrix &dddD,
+                                            floatMatrix &dddF, floatMatrix &dddchi, floatMatrix &dddchiNL,
+                                            floatMatrix &d2ddFdXi_1, floatMatrix &d2ddchidXi_1,
+                                            floatMatrix &d2ddFdXi_2, floatMatrix &d2ddchiNLdXi_2,
+                                            floatMatrix &d2ddFdD );
+
     errorOut decomposeVector( const floatVector &d, const floatVector &n,
                               floatVector &dn, floatVector &dt );
 
